6fe510ab132ea6c52b1f3c9d8e08c95d628cc121
jamminP <parkjaemin08@gmail.com>
2025-04-17 14:28:01 +0900

    자바스크립트 1회차 공부

---
+++ JavaScript-1/1회차/과제1.js
@@ -0,0 +1,68 @@
+/**
+ * JavaScript 1일차 과제
+ * 동물 출력하기
+ * 
+ * 출력 확인법
+ * Terminal 창에 다음 코드를 작성해주세요.
+ * node main
+ * 
+ * 아래 내용을 console.log를 활용하여 Terminal에 출력해주세요.
+ * 
+ * 
+고양이
+ /\_/\  
+( o.o ) 
+ > ^ <
+ 
+토끼
+ (\_/)
+ ( •_•)
+ />🌹
+ 
+강아지
+ /^ ^\
+/ 0 0 \
+V\ Y /V
+ / - \
+ |    \
+ || (__)
+ 
+병아리
+(•ө•)
+(   )>
+ " "
+
+오리
+<(' )
+ (   )
+  ^^
+ */
+
+// 여기에 코드를 작성해주세요.
+console.log('고양이');
+console.log(' /\\_/\\');
+console.log('( o.o ) ');
+console.log(' > ^ <');
+
+console.log('토끼');
+console.log(' (\\_/)');
+console.log(' ( •_•)');
+console.log(' />🌹');
+
+console.log('강아지');
+console.log(' /^ ^\\');
+console.log('/ 0 0 \\');
+console.log('V\\ Y /V');
+console.log(' / - \\');
+console.log(' |    \\');
+console.log(' || (__)');
+
+console.log('병아리');
+console.log('(•ө•)');
+console.log('(   )>');
+console.log(' " "');
+
+console.log('오리');
+console.log("<(' )");
+console.log(' (   )');
+console.log('  ^^');
